c8613a29a7114456755b80a1938e34f0a5d5806d
PREDATOR <PREDATOR@Sima-PC>
2026-01-09 14:16:09 +0300

    test commit

--- src/musicStreaming/Main.java
+++ src/musicStreaming/Main.java
@@ -15,6 +15,9 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Simaa");
+        System.out.println("Simaa");
+        System.out.println("Simaa");
+        System.out.println("Simaa");
     }
     
 }
